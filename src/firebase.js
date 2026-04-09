04e98433007b8e48e00b7a48c6bde508200d26be
Giovacchio <giovacchioxq@gmail.com>
2026-04-10 01:44:36 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// ⚠️ INCOLLA QUI LE TUE CREDENZIALI FIREBASE (vedi guida nel README)
 const firebaseConfig = {
-  apiKey: "INCOLLA_QUI",
-  authDomain: "INCOLLA_QUI",
-  projectId: "INCOLLA_QUI",
-  storageBucket: "INCOLLA_QUI",
-  messagingSenderId: "INCOLLA_QUI",
-  appId: "INCOLLA_QUI",
+  apiKey: "AIzaSyA5DhwlylgKVgurB6eK1fK-GV3Iv1RT-G8",
+  authDomain: "covo-di-cthulhu.firebaseapp.com",
+  databaseURL: "https://covo-di-cthulhu-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "covo-di-cthulhu",
+  storageBucket: "covo-di-cthulhu.firebasestorage.app",
+  messagingSenderId: "1002748799651",
+  appId: "1:1002748799651:web:16de0efcaf251803e6ed16",
+  measurementId: "G-S5VDCHP1J7"
 };
 
 const app = initializeApp(firebaseConfig);
